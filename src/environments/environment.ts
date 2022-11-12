171ff2701b5b2eb1c803a30b2535e4d9b193e874
Yurii Deneha <yurii@squareball.co>
2022-11-12 12:16:34 +0200

    Firebase connection

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAHrnYh3D4dCTDp0qjHCFI2L6Yotj_FjQU",
+    authDomain: "capusta-app.firebaseapp.com",
+    projectId: "capusta-app",
+    storageBucket: "capusta-app.appspot.com",
+    messagingSenderId: "775229439724",
+    appId: "1:775229439724:web:e1a7ab5af812b712aeafc9"
+  }
 };
 
 /*
